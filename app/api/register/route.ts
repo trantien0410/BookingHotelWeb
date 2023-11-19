f54ede78edd9d00474a4dc282e0e2f6d7aaf8d60
Tien <tien04100@gmail.com>
2023-11-19 21:56:55 +0700

    modify register api

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -2,16 +2,28 @@ import bcrypt from "bcrypt";
 
 import prisma from "@/app/libs/prismadb";
 import { NextResponse } from "next/server";
+import { Role } from "@prisma/client";
 
 export async function POST(request: Request) {
   const body = await request.json();
   const { email, name, password } = body;
+
+  const existingUser = await prisma.user.findUnique({
+    where: { email },
+  });
+
+  if (existingUser) {
+    // Return an error response if a user with the same email exists
+    return NextResponse.error();
+  }
+
   const hashedPassword = await bcrypt.hash(password, 12);
 
   const user = await prisma.user.create({
     data: {
       email,
       name,
+      role: Role.USER,
       hashedPassword,
     },
   });

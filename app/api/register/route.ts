4dc755a8e2ec6959e4797a82f3f0d849d68898ad
Tien <tien04100@gmail.com>
2023-11-20 10:56:30 +0700

    modify register route

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -7,16 +7,6 @@ import { Role } from "@prisma/client";
 export async function POST(request: Request) {
   const body = await request.json();
   const { email, name, password } = body;
-
-  const existingUser = await prisma.user.findUnique({
-    where: { email },
-  });
-
-  if (existingUser) {
-    // Return an error response if a user with the same email exists
-    return NextResponse.error();
-  }
-
   const hashedPassword = await bcrypt.hash(password, 12);
 
   const user = await prisma.user.create({
